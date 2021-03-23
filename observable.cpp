b5f64c7ce8c7ca63d2654a3902d3f66bb9db986a
Joren Brunekreef <jorenb@gmail.com>
2021-03-23 13:36:28 +0100

    improve breadth first search

--- observable.cpp
+++ observable.cpp
@@ -9,17 +9,17 @@ std::default_random_engine Observable::rng(0);  // TODO(JorenB): seed properly
 void Observable::write() {
     std::string filename = data_dir + name + "-" + identifier + extension;
 
-	std::ifstream infile(filename);
-	if (!infile.good()) { printf("output file deleted\n"); exit(1); }
-	infile.close();
+    std::ifstream infile(filename);
+    if (!infile.good()) { printf("output file deleted\n"); exit(1); }
+    infile.close();
 
     std::ofstream file;
     file.open(filename, std::ios::out | std::ios::app);
 
-	assert(file.is_open());
+    assert(file.is_open());
 
-	file << output << "\n";
-	file.close();
+    file << output << "\n";
+    file.close();
 }
 
 void Observable::clear() {
@@ -28,17 +28,23 @@ void Observable::clear() {
     std::ofstream file;
     file.open(filename, std::ios::out | std::ios::trunc);
 
-	assert(file.is_open());
+    assert(file.is_open());
 
-	file.close();
+    file.close();
 }
 
 std::vector<Vertex::Label> Observable::sphere(Vertex::Label origin, int radius) {
-	std::vector<Vertex::Label> done;
+    std::vector<bool> done;
     std::vector<Vertex::Label> thisDepth;
     std::vector<Vertex::Label> nextDepth;
 
-    done.push_back(origin);
+    int vmax = 0;
+    for (auto v : Universe::vertices) {
+        if (v > vmax) vmax = v;
+    }
+    done.resize(vmax + 1, false);
+
+    done.at(origin) = true;
     thisDepth.push_back(origin);
 
     std::vector<Vertex::Label> vertexList;
@@ -46,11 +52,11 @@ std::vector<Vertex::Label> Observable::sphere(Vertex::Label origin, int radius) 
     for (int currentDepth = 0; currentDepth < radius; currentDepth++) {
         for (auto v : thisDepth) {
             for (auto neighbor : Universe::vertexNeighbors[v]) {
-               if (std::find(done.begin(), done.end(), neighbor) == done.end()) {
-                   nextDepth.push_back(neighbor);
-                   done.push_back(neighbor);
-                   if (currentDepth == radius - 1) vertexList.push_back(neighbor);
-               }
+                if (!done.at(neighbor)) {
+                    nextDepth.push_back(neighbor);
+                    done.at(neighbor) = true;
+                    if (currentDepth == radius - 1) vertexList.push_back(neighbor);
+                }
             }
         }
         thisDepth = nextDepth;
@@ -61,11 +67,17 @@ std::vector<Vertex::Label> Observable::sphere(Vertex::Label origin, int radius) 
 }
 
 std::vector<Triangle::Label> Observable::sphereDual(Triangle::Label origin, int radius) {
-	std::vector<Triangle::Label> done;
+    std::vector<bool> done;
     std::vector<Triangle::Label> thisDepth;
     std::vector<Triangle::Label> nextDepth;
 
-    done.push_back(origin);
+    int tmax = 0;
+    for (auto t : Universe::triangles) {
+        if (t > tmax) tmax = t;
+    }
+    done.resize(tmax + 1, false);
+
+    done.at(origin) = true;
     thisDepth.push_back(origin);
 
     std::vector<Triangle::Label> triangleList;
@@ -73,11 +85,11 @@ std::vector<Triangle::Label> Observable::sphereDual(Triangle::Label origin, int 
     for (int currentDepth = 0; currentDepth < radius; currentDepth++) {
         for (auto t : thisDepth) {
             for (auto neighbor : Universe::triangleNeighbors[t]) {
-               if (std::find(done.begin(), done.end(), neighbor) == done.end()) {
-                   nextDepth.push_back(neighbor);
-                   done.push_back(neighbor);
-                   if (currentDepth == radius-1) triangleList.push_back(neighbor);
-               }
+                if (!done.at(neighbor)) {
+                    nextDepth.push_back(neighbor);
+                    done.at(neighbor) = true;
+                    if (currentDepth == radius-1) triangleList.push_back(neighbor);
+                }
             }
         }
         thisDepth = nextDepth;
@@ -90,55 +102,71 @@ std::vector<Triangle::Label> Observable::sphereDual(Triangle::Label origin, int 
 int Observable::distance(Vertex::Label v1, Vertex::Label v2) {
     if (v1 == v2) return 0;
 
-    std::vector<Vertex::Label> done;
+    std::vector<bool> done;
     std::vector<Vertex::Label> thisDepth;
     std::vector<Vertex::Label> nextDepth;
 
-    done.push_back(v1);
+    int vmax = 0;
+    for (auto v : Universe::vertices) {
+        if (v > vmax) vmax = v;
+    }
+    done.resize(vmax + 1, false);
+
+    done.at(v1) = true;
     thisDepth.push_back(v1);
 
     int currentDepth = 0;
-    while (true) {
+    do {
         for (auto v : thisDepth) {
             for (auto neighbor : Universe::vertexNeighbors[v]) {
                 if (neighbor == v2) return currentDepth+1;
 
-                if (std::find(done.begin(), done.end(), neighbor) == done.end()) {
+                if (!done.at(neighbor)) {
                     nextDepth.push_back(neighbor);
-                    done.push_back(neighbor);
+                    done.at(neighbor) = true;
                 }
             }
         }
         thisDepth = nextDepth;
         nextDepth.clear();
         currentDepth++;
-    }
+    } while (thisDepth.size() > 0);
+
+    return -1;  // should not happen
 }
 
 int Observable::distanceDual(Triangle::Label t1, Triangle::Label t2) {
     if (t1 == t2) return 0;
 
-    std::vector<Triangle::Label> done;
+    std::vector<bool> done;
     std::vector<Triangle::Label> thisDepth;
     std::vector<Triangle::Label> nextDepth;
 
-    done.push_back(t1);
+    int tmax = 0;
+    for (auto t : Universe::triangles) {
+        if (t > tmax) tmax = t;
+    }
+    done.resize(tmax + 1, false);
+
+    done.at(t1) = true;
     thisDepth.push_back(t1);
 
     int currentDepth = 0;
-    while (true) {
+    do {
         for (auto t : thisDepth) {
             for (auto neighbor : Universe::triangleNeighbors[t]) {
                 if (neighbor == t2) return currentDepth + 1;
 
-                if (std::find(done.begin(), done.end(), neighbor) == done.end()) {
+                if (!done.at(neighbor)) {
                     nextDepth.push_back(neighbor);
-                    done.push_back(neighbor);
+                    done.at(neighbor) = true;
                 }
             }
         }
         thisDepth = nextDepth;
         nextDepth.clear();
         currentDepth++;
-    }
+    } while (thisDepth.size() > 0);
+
+    return -1;  // should not happen
 }
